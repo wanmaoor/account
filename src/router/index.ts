80f1238008c88deccbe2f09ae46ca7edd05ed8dd
wanmaoor <wanmao1284879375@outlook.com>
2020-01-19 11:16:00 +0800

    添加404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import VueRouter from "vue-router"
 import Money from "@/views/Money.vue"
 import Labels from "@/views/Labels.vue"
 import Statistics from "@/views/Statistics.vue"
+import NotFound from "@/views/NotFound.vue"
 
 Vue.use(VueRouter)
 
@@ -22,6 +23,10 @@ const routes = [
   {
     path: "/statistics",
     component: Statistics
+  },
+  {
+    path: "*",
+    component: NotFound
   }
 ]
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    当前页面不存在,
+    <router-link to="/">返回首页</router-link>
+  </div>
+</template>
+
+<script>
+	export default {
+		name: "NotFound"
+	}
+</script>
+
+<style scoped>
+
+</style>
