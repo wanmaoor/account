04d843c8df40512269a05fc8bd30e84eb5d6fde0
wanmaoor <wanmao1284879375@outlook.com>
2020-02-04 10:21:42 +0800

    自动删除svg的fill属性

--- vue.config.js
+++ vue.config.js
@@ -10,6 +10,8 @@ module.exports = {
 			.test(/\.svg$/)
 			.include.add(dir).end() // 包含 icons 目录
 			.use("svg-sprite-loader").loader("svg-sprite-loader").options({extract: false}).end()
+			.use("svgo-loader").loader("svgo-loader")
+			.tap(options => ({...options, plugins: [{removeAttrs: {attrs: "fill"}}]})).end()
 		config.plugin("svg-sprite").use(require("svg-sprite-loader/plugin"), [{plainSprite: true}])
 		config.module.rule("svg").exclude.add(dir) // 其他 svg loader 排除 icons 目录
 	}
