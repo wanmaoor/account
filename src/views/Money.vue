d87aa2d6123294a9820e990b158abad7fca0a16a
wanmaoor <wanmao1284879375@outlook.com>
2020-02-06 17:05:21 +0800

    增加Money组件的标签, 备注样式

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,47 @@
 <template>
   <Layout>
-    <p>money</p>
+    <div class="tags">
+      <div class="new">
+        <button>新增标签</button>
+      </div>
+      <ul class="current">
+        <li>衣</li>
+        <li>食</li>
+        <li>住</li>
+        <li>行</li>
+      </ul>
+    </div>
+    <div>
+      <label class="notes">
+        <span class="name">备注</span>
+        <input id="" name="" placeholder="请输入备注" type="text">
+      </label>
+    </div>
+    <div>
+      <ul class="types">
+        <li class="selected">支出</li>
+        <li>收入</li>
+      </ul>
+    </div>
+    <div class="numberPad">
+      <div class="output">100</div>
+      <div class="buttons">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>删除</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>清空</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>OK</button>
+        <button>0</button>
+        <button>.</button>
+      </div>
+    </div>
   </Layout>
 </template>
 
@@ -11,6 +52,106 @@
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  @import "~@/assets/style/global.scss";
 
-</style>
+  .tags {
+    font-size: 14px;
+    padding: 16px;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column-reverse;
+
+    > .current {
+      display: flex;
+      flex-wrap: wrap;
+
+      > li {
+        background: #d9d9d9;
+        $h: 24px;
+        height: $h;
+        line-height: $h;
+        border-radius: $h/2;
+        padding: 0 16px;
+        margin-right: 12px;
+        margin-top: 4px;
+      }
+    }
+
+    > .new {
+      padding-top: 16px;
+
+      button {
+        background: transparent;
+        border: none;
+        color: #999;
+        border-bottom: 1px solid;
+        padding: 0 4px;
+      }
+    }
+  }
+
+  .notes {
+    background: #f5f5f5;
+    font-size: 14px;
+    padding-left: 16px;
+    display: flex;
+    align-items: center;
+
+    .name {
+      padding-right: 16px;
+    }
+
+    input {
+      height: 64px;
+      background: transparent;
+      border: none;
+      flex-grow: 1;
+      padding-right: 16px;
+    }
+  }
+
+  .types {
+    background: #c4c4c4;
+    display: flex;
+    text-align: center;
+    font-size: 24px;
+
+    > li {
+      width: 50%;
+      height: 64px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+
+      &.selected::after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        height: 4px;
+        width: 100%;
+        background: #333;
+      }
+    }
+  }
+
+  .numberPad {
+    .output {
+      font-size: 36px;
+      font-family: Consolas, monospace;
+      padding: 9px 16px;
+      text-align: right;
+    }
+
+    .buttons {
+      display: flex;
+      flex-wrap: wrap;
+
+      > button {
+        width: 25%;
+      }
+    }
+  }
+</style> 
