721017c8f5a42076f581761a79dc60d4b15aeb69
wanmaoor <wanmao1284879375@outlook.com>
2020-02-09 21:36:29 +0800

    Labels.vue > 完成SCSS样式

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,22 +1,26 @@
 <template>
   <Layout>
     <ol class="tagList">
-      <li><span>衣</span>
+      <li>
+        <span>衣</span>
         <Icon name="right"/>
       </li>
-      <li><span>食</span>
+      <li>
+        <span>食</span>
         <Icon name="right"/>
       </li>
-      <li><span>住</span>
+      <li>
+        <span>住</span>
         <Icon name="right"/>
       </li>
-      <li><span>行</span>
+      <li>
+        <span>行</span>
         <Icon name="right"/>
       </li>
-      <div>
-        <button>新建标签</button>
-      </div>
     </ol>
+    <div class="createTag-Wrapper">
+      <button class="createTag">新建标签</button>
+    </div>
   </Layout>
 </template>
 
@@ -31,9 +35,37 @@
 <style lang="scss" scoped>
   .tagList {
     background: white;
+    font-size: 16px;
+    padding-left: 16px;
 
     > li {
+      min-height: 64px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      border-bottom: 1px solid #e6e5e8;
 
+      svg {
+        margin-right: 16px;
+        width: 18px;
+        height: 18px;
+        color: #333333;
+      }
+    }
+  }
+
+  .createTag {
+    background: cornflowerblue;
+    color: cornsilk;
+    border-radius: 4px;
+    border: none;
+    height: 40px;
+    padding: 0 16px;
+
+    &-Wrapper {
+      text-align: center;
+      padding: 16px;
+      margin-top: 28px;
     }
   }
 </style>
