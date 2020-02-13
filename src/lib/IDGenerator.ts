a6aedbc89d783d85539f4d34e865fe471c43f772
wanmaoor <wanmao1284879375@outlook.com>
2020-02-13 17:38:48 +0800

    Bug fixed: fix bug at IDGenerator.ts

--- src/lib/IDGenerator.ts
+++ src/lib/IDGenerator.ts
@@ -2,6 +2,7 @@ let id: number = parseInt(window.localStorage.getItem("_lastId") || "0") || 0
 
 function IDGenerator() {
   id++
+  window.localStorage.setItem("_lastId", id.toString())
   return id
 }
 
