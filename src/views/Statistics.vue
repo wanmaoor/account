720cfad653567c521dda4d52624d20af0a1a3b38
wanmaoor <wanmao1284879375@outlook.com>
2020-02-21 22:52:48 +0800

    完成统计视图的统计Tab和时间Tab

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,14 +1,41 @@
 <template>
-  <Layout>统计</Layout>
+  <Layout>
+      <Tab
+        active="-"
+        onlyHeader
+        noBar
+        class="tab1"
+      >
+        <TabPanel value="-" label="支出"/>
+        <TabPanel value="+" label="收入"/>
+      </Tab>
+      <Tab
+        active="day"
+        class="tab2"
+      >
+        <TabPanel value="day" label="天">day day day</TabPanel>
+        <TabPanel value="week" label="周">week week week</TabPanel>
+        <TabPanel value="month" label="月">month month month</TabPanel>
+      </Tab>
+  </Layout>
 </template>
 
-<script>
+<script lang="ts">
+  import {Component, Vue} from "vue-property-decorator"
+  import {Tab, TabPanel} from "@wanmaoor/giaoui"
 
-	export default {
-		name: "Statistics",
-	}
+  @Component({
+    components: {Tab, TabPanel}
+  })
+  export default class Statistics extends Vue {
+  }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  .tab1 ::v-deep .tab-header{
+     font-size: 1.5rem;
+   }
+  .tab2 ::v-deep .tab-header{
+    font-size: 1rem;
+  }
 </style>
